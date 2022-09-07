28534fa7cdb314e7ae8e958125c623c57be192cc
kashiken4869 <kashiken4646@gmail.com>
2022-09-07 22:32:02 +0900

    #19 通知機能 Issue3 イベントの前日にメールで通知する 通知する先は参加としている人のみ#19

--- src/sendmail.php
+++ src/sendmail.php
@@ -5,45 +5,42 @@ date_default_timezone_set('Asia/Tokyo');
 mb_language('ja');
 mb_internal_encoding('UTF-8');
 
+$start_date  = date('Y-m-d 00:00:00', strtotime("+1 day"));
+$end_date  = date('Y-m-d 23:59:59', strtotime("+1 day"));
 
-$stmt = $db->prepare("SELECT * FROM users");
+$stmt = $db->prepare("SELECT events.start_at, events.name AS event_name, events.detail, users.email, users.name FROM events
+LEFT JOIN event_attendance ON event_attendance.event_id = events.id 
+LEFT JOIN users ON event_attendance.user_id = users.id
+WHERE '$start_date' < events.start_at 
+AND events.start_at < '$end_date' 
+AND event_attendance.status = 1
+");
 $stmt->execute();
 $users = $stmt->fetchAll();
 
-// $date  = date("Ymd",strtotime("-1 day"));
-$start_date  = date('Y-m-d 00:00:00',strtotime("+1 day"));
-$end_date  = date('Y-m-d 23:59:59',strtotime("+1 day"));
+    foreach ($users as $user) {
 
-$stmt2 = $db->prepare("SELECT * FROM events where '$start_date' < start_at AND start_at < '$end_date'");
-$stmt2->execute();
-$events = $stmt2->fetchAll();
+        $to = $user['email'];
+        $subject = "posseイベント前日メール";
+        $body = "本文";
+        $headers = ["From" => "system@posse-ap.com", "Content-Type" => "text/plain; charset=UTF-8", "Content-Transfer-Encoding" => "8bit"];
 
-foreach ($events as $event) {
-foreach ($users as $user) {
-
-$to = $user['email'];
-$subject = "posseイベント前日メール" ;
-$body = "本文";
-$headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
-
-$name = $user['name'];
-$date = $event['start_at'];
-$event_name = $event['name'];
-$detail = $event['detail'];
-$body = <<<EOT
+        $name = $user['name'];
+        $date = $user['start_at'];
+        $event_name = $user['event_name'];
+        $detail = $user['detail'];
+        $body = <<<EOT
 {$name}さん
 
-${date}に${event_name}を開催します。
+参加予定の「${event_name}」の前日となりました。
+${date}に始まります！
 
-イベント内容↓↓↓
+【イベント内容】
 ${detail}
-
-ぜひ来てください！！！
 
 EOT;
 
-mb_send_mail($to, $subject, $body, $headers);
-}
-}
-echo "メールを送信しました";
+        mb_send_mail($to, $subject, $body, $headers);
+    }
 
+echo "メールを送信しました";
