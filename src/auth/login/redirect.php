2c30bddc097e3e270ffdd05381456ba82fca3cc9
karen <caremayo812@icloud.com>
2022-09-08 08:53:15 +0900

    github連携のためのトークンを取得

--- src/auth/login/redirect.php
+++ src/auth/login/redirect.php
@@ -1,10 +1,7 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . "/dbconnect.php");
+require( "../../dbconnect.php");
 session_start();
 
-$dsn = 'mysql:host=mysql;dbname=posse;charset=utf8;';
-$user = 'posse_user';
-$password = 'password';
 
 /**
  * HTTPリクエスト用の関数
@@ -45,45 +42,22 @@ $POST_DATA = array(
     'code'          => $code,
 );
 
-// 3. アクセストークンの取得
+// 3. アクセストークンの取得→返却値をJsonでデコード
 $resultAT = httpRequest('post', "https://github.com/login/oauth/access_token", $POST_DATA, ["Accept: application/json"]);
-//  APIでユーザのEmail情報を取得
-$resultEmail   = httpRequest('get', "https://api.github.com/user/emails", null, ["Authorization: Token " . "アクセストークン"]);
-
-// 返却地をJsonでデコード
 $resJsonAT = json_decode($resultAT, true);
-// 返却地をJsonでデコード
-$resJsonEmails = json_decode($resultEmail, true);
 
 // アクセストークン
-echo $resJsonAT['access_token'];
-echo $resJsonEmails[0]['email'];
+// echo $resJsonAT['access_token'];
 
-// 4. APIでユーザ情報の取得
+// 4. APIでユーザ情報の取得→返却値をJsonでデコード
 $resultUser = httpRequest('get', "https://api.github.com/user", null, ["Authorization: Token " . $resJsonAT['access_token']]);
-// 返却地をJsonでデコード
 $resJsonUser = json_decode($resultUser, true);
-
-// ユーザ情報
-var_dump($resJsonUser);
-// echo $resJsonUser[0]["name"];
-$git_id = $resJsonUser[0]["login"];
-echo $git_id;
+// var_dump($resJsonUser);
+$git_id = $resJsonUser["login"];
 $git_name = $resJsonUser["name"];
-$git_email = $resJsonEmails['email'];
 
-// DB登録処理とか
-// 〜〜〜もし既にある情報と一致してなかったら新規登録〜〜〜
-$db = new PDO($dsn, $user, $password);
-$stmt = $db->prepare("INSERT INTO users (email, name, login_pass, slack_id, github_id, role_id) VALUES (:email, :name, :login_pass, :slack_id, :github_id, :role_id)");
-$stmt->execute(array(':email' => $git_email, ':name' => $git_name, ':login_pass' => password_hash('password', PASSWORD_DEFAULT), ':slack_id' => 123456, ':github_id' => $git_id,  ':role_id' =>'1'));
+//  APIでユーザのEmail情報を取得→返却値をJsonでデコード
+$resultEmail   = httpRequest('get', "https://api.github.com/user/emails", null, ["Authorization: Token " . $resJsonAT['access_token']]);
+$resJsonEmails = json_decode($resultEmail, true);
+$git_email = $resJsonEmails[0]['email'];
 
-
-if (isset($git_id)) {
-    session_regenerate_id(TRUE); //セッションidを再発行
-    $_SESSION["user_id"] = 123456; //仮
-    $_SESSION["role_id"] = 1;
-    $_SESSION['name'] = $git_name;
-    $_SESSION["login"] = $git_email; //セッションにログイン情報を登録
-    header('Location: ../../index.php');
-}
