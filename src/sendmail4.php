71ee90fab241a2abea4e29160f7ed169f6902eb9
kashiken4869 <kashiken4646@gmail.com>
2022-09-08 20:51:52 +0900

    webhook url update

--- src/sendmail4.php
+++ src/sendmail4.php
@@ -54,7 +54,7 @@ EOT;
 
     mb_send_mail($to, $subject, $body, $headers);
 
-$url = "https://hooks.slack.com/services/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";
+$url = "https://hooks.slack.com/services/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";
 
 // メッセージ
 if ( $slack_id != NULL ) {
